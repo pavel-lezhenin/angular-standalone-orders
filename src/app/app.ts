624615d658680f96427b5e32998020a2d3e2a06d
Front Templates Dev <dev@front-templates.local>
2026-02-06 21:46:42 +0100

    fix: format code and fix linting issues for Angular project

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,8 +5,9 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
+// eslint-disable-next-line @angular-eslint/component-class-suffix
 export class App {
   protected readonly title = signal('angular-standalone-orders');
 }
